48fa9c0786125488ae62b5287f667a03cc874c1e
yaoqi <1159991642@qq.com>
2016-04-01 14:18:52 +0800

    Initial Commit

---
+++ CustomAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertView
+//
+//  Created by yaoqi on 16/4/1.
+//  Copyright © 2016年 微贷（杭州）金融信息服务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomAlertView
+//
+//  Created by yaoqi on 16/4/1.
+//  Copyright © 2016年 微贷（杭州）金融信息服务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
